af6bc36b0d2c26df28c25d029e3d012cc6741931
Jinsol <js3891129@gmail.com>
2021-06-15 11:37:32 +0900

    update directoring

---
+++ boardgame/src/pages/akinator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Akinator: React.FC = () => {
+  return (
+    <>
+    ㅎㅇd
+    </>
+  )
+}
+
+export default Akinator
---
+++ boardgame/src/pages/detail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Detail = () => {
+  return (
+    <>
+    안농
+    </>
+  )
+}
+
+export default Detail
