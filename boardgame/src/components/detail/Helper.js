04fbda10d94eb472ca5c08de4dca2bfdb1c5c205
Jinsol <js3891129@gmail.com>
2021-06-17 16:12:29 +0900

    Revert "Merge branch 'main' of https://github.com/Seongnam-Citizen-Developers/scaling-journey"
    This reverts commit 9ac812ed0a65ba1683ce3d7b7a195d9e7a142f5c, reversing
    changes made to b1c0e98dd7fbe7d1ada15e5b5a6b9054edd77dfa.

--- boardgame/src/components/detail/Helper.js
+++ boardgame/src/components/detail/Helper.js
@@ -1,23 +1,10 @@
-import _ from 'lodash'
-
 export function getgameDetail(game){
+  console.log('겟겜디',game)
   const image_url = game.image_url
   const pattern = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
   const name = game.tags.filter(tag =>{
     return pattern.test(tag)
   })
+  console.log('name',name)
   return {image_url:image_url,name:name[0]}
-}
-
-export function reviewPager(reviews){
-  console.log(reviews)
-  let newArray = []
-  // for(let i =0 ; i<=reviews.length ; i+=3){
-  //   console.log('slice',reviews.slice(i,i+3))
-  //   console.log('i: ',i,'i.length: ',reviews.length)
-  //   newArray.push(reviews.slice(i,i+3))
-  // }
-  newArray = _.chunk(reviews,3)
-  console.log('newArray',newArray)
-  return {'pageReviews':newArray,'length': newArray.length}
 }
