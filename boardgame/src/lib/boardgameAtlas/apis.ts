0148937b31772ae7e043cccfd6079c1def8ad932
Jinsol <js3891129@gmail.com>
2021-06-17 18:03:51 +0900

    buildfix

--- boardgame/src/lib/boardgameAtlas/apis.ts
+++ boardgame/src/lib/boardgameAtlas/apis.ts
@@ -12,6 +12,7 @@ export const getBoardgames = async (params: any): Promise<game[]> => {
   const config: AxiosRequestConfig = {
     params: {...params, client_id: process.env.REACT_APP_CLIENT_ID}
   }
+  
   const res = await axios.get<resGetBoardgameApi>(`${URLS.boardgame}/search`, config)
   return res.data.games
 }
