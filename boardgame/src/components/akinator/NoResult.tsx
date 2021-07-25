20e6d0d8c4589fbe6a01d767f95bd3bf6329b56a
SpookyJelly <74237436+SpookyJelly@users.noreply.github.com>
2021-07-25 16:59:44 +0900

    fixed user interface design

--- boardgame/src/components/akinator/NoResult.tsx
+++ boardgame/src/components/akinator/NoResult.tsx
@@ -1,29 +1,31 @@
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { game } from '../../lib/boardgameAtlas/interfaces'
-import { getgameDetail } from '../detail/Helper'
+import { useEffect } from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { game } from "../../lib/boardgameAtlas/interfaces";
+import { getgameDetail } from "../detail/Helper";
+import { CircularProgress } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 
 interface NoResultProps {
-  games: game[]
+  games: game[];
 }
 
 function getRandomIndexes(games: game[]): number[] {
-  let length = games.length
-  let array: number[] = []
+  let length = games.length;
+  let array: number[] = [];
   if (length < 4) {
     for (let i = 0; i < length; i++) {
-      array.push(i)
+      array.push(i);
     }
   } else {
     while (array.length !== 3) {
-      let randomInt = Math.floor(Math.random() * length)
+      let randomInt = Math.floor(Math.random() * length);
       if (!array.includes(randomInt)) {
-        array.push(randomInt)
+        array.push(randomInt);
       }
     }
   }
-  return array
+  return array;
 }
 
 const NoResult: React.FC<NoResultProps> = ({ games }) => {
@@ -32,13 +34,19 @@ const NoResult: React.FC<NoResultProps> = ({ games }) => {
   //   setMechanics(data.currentTarget.value)
   //   setPhase('request')
   // }
-  const [randomIndexes, setRandomIndexes] = useState<number[]>([])
+  const style = useStyles();
+  const [randomIndexes, setRandomIndexes] = useState<number[]>([]);
   useEffect(() => {
-    setRandomIndexes(getRandomIndexes(games))
-  }, [games])
-  const history = useHistory()
+    setRandomIndexes(getRandomIndexes(games));
+  }, [games]);
+  const history = useHistory();
   return games.length === 0 ? (
-    <></>
+    <>
+      <div className={style.loadingArea}>
+        <h2 className={style.text}>잠시만 기다려주세요</h2>
+        <CircularProgress />
+      </div>
+    </>
   ) : (
     <>
       <p>정확히 일치하는 게임이 없어요 ㅠㅠ</p>
@@ -48,15 +56,24 @@ const NoResult: React.FC<NoResultProps> = ({ games }) => {
         <div
           key={index}
           onClick={() => {
-            history.push(`/detail/${games[randomIndex].id}`)
+            history.push(`/detail/${games[randomIndex].id}`);
           }}
         >
           <img src={games[randomIndex].image_url} alt="" width="40%" />
-          {getgameDetail(games[randomIndex]).name}
+          <p>{getgameDetail(games[randomIndex]).name}</p>
         </div>
       ))}
     </>
-  )
-}
+  );
+};
 
-export default NoResult
+export default NoResult;
+
+const useStyles = makeStyles({
+  loadingArea: {
+    textAlign: "center",
+  },
+  text: {
+    textAlign: "center",
+  },
+});
--- boardgame/src/pages/Index.tsx
+++ boardgame/src/pages/Index.tsx
@@ -3,47 +3,41 @@ import { useHistory } from "react-router";
 import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 
-//test : 
+//test :
 // #2E294E // #9055A2 // #D499B9
 const useStyles = makeStyles({
-    main : {
-        textAlign:'center',
-        padding : "10% 3% 10% 3%",
-    },
-    title : {
-    },
-    detail :{
-        padding : "10% 3% 10% 3%",
-    },
-})
+  main: {
+    textAlign: "center",
+    padding: "10% 3% 10% 3%",
+  },
+  title: {},
+  detail: {
+    padding: "10% 3% 10% 3%",
+  },
+});
 
-const Index: React.FC = () =>{
-    
-    const history = useHistory()
-    const classes = useStyles()
+const Index: React.FC = () => {
+  const history = useHistory();
+  const classes = useStyles();
 
-    const onClick = () =>{
-        history.push('/akinator')
-    }
+  const onClick = () => {
+    history.push("/akinator");
+  };
 
-    return(
-        <div
-            className={classes.main}
-        >
-            <h1 className={classes.title}>프로젝트 이름 </h1>
-            <div className={classes.detail}>
-                <p>무슨 보드게임을 해야할지 모르겠다고요??</p>
-                <p>저희가 딱 알맞는 게임을 추천해드릴께요!!</p>
-                <p>고민은 줄이고, 즐거움만 잔뜩 누리세요!</p>
-            </div>
-                    
-            <Button 
-                variant="contained" 
-                color="primary"
-                onClick={onClick}
-            >지금 바로 시작하기</Button>
-        </div>
-    )
-}
+  return (
+    <div className={classes.main}>
+      <h1 className={classes.title}>솔로몬 </h1>
+      <div className={classes.detail}>
+        <p>재미있는 보드게임을 하고 싶으시다고요?</p>
+        <p>저희가 추천해드릴께요!!</p>
+        <p>고민은 줄이고, 즐거움만 잔뜩 누리세요!</p>
+      </div>
 
-export default Index
+      <Button variant="contained" color="primary" onClick={onClick}>
+        시작하기
+      </Button>
+    </div>
+  );
+};
+
+export default Index;
